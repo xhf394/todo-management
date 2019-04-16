1893f1be32af5bd9fd7818de5b7db7ede9f1eb67
xhf394 <39177879+xhf394@users.noreply.github.com>
2019-04-16 17:41:08 +0800

    array.map() is very powerful

--- src/Components/TheRoad/TheRoad.js
+++ src/Components/TheRoad/TheRoad.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
-
+import './theRoad.css';
 
 
 class TheRoad extends Component {
   render() {
+  	  //start insert
+      const helloWorld = 'Welcome to the Road to learn React';
+
+      //add a array list for react related source
+      //always use 'const' if the data strcuture won't change
+      const list = [
+      	{
+      	  title: 'React',
+      	  url:'https://reactjs.org/',
+      	  author: 'Jordan Walke',
+      	  num_comments: 3,
+      	  points: 4,
+      	  objectID: 0,
+      },
+        {
+          title: 'Redux',
+          url: 'https://redux.js.org/',
+          author: 'Dan Abramov, Andrew Clark',
+          num_comments: 2,
+          points: 5,
+          objectID: 1,
+        }
+      ];
+
     return (
+
       <div className="theRoad">
-      	The Road to React
-      	
+      	<h2> {helloWorld}</h2>
+      	<div>
+      	  {list.map((item, itemIndex) => {
+      		return <div key={itemIndex}>{item.title} </div>
+      	})}
+      	</div>  
       </div>
     );
   }
