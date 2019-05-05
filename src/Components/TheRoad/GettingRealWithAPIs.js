b38526ac2226d5a6538ce02d0852f07e24fd1895
xhf394 <39177879+xhf394@users.noreply.github.com>
2019-05-05 14:44:03 +0800

    add API test section

---
+++ src/Components/TheRoad/GettingRealWithAPIs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class gettingRealWithAPIs extends Component {
+
+  render() {
+    return (
+      <div>
+      gettingRealWithAPIs
+      </div>
+	)
+  }
+}
+
+export default gettingRealWithAPIs;
