1c729f134a4ea935cd412818ba23b36bcc4c41d2
xhf394 <39177879+xhf394@users.noreply.github.com>
2019-05-06 17:02:16 +0800

    fetch data structure update

--- src/Components/TheRoad/GettingRealWithAPIs.js
+++ src/Components/TheRoad/GettingRealWithAPIs.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
+
+//define API address
+const DEFAULT_QUERY = 'redux';
+//keep URL composition flexible
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+//whole address
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
+console.log(url);
+
+
 class gettingRealWithAPIs extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+    	result: null,
+    	searchTerm: DEFAULT_QUERY,
+    }
+  }
 
   render() {
     return (
@@ -12,3 +34,4 @@ class gettingRealWithAPIs extends Component {
 }
 
 export default gettingRealWithAPIs;
+
