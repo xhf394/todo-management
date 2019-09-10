2b4cc0a5cfa8fd770c938c680691e2ffbb47f288
xhf394 <39177879+xhf394@users.noreply.github.com>
2019-09-10 22:20:41 +0800

    update constants

--- src/Components/ReactComponentRefactor/constants/index.js
+++ src/Components/ReactComponentRefactor/constants/index.js
@@ -1,7 +1,7 @@
 //define API address
 export const DEFAULT_QUERY = 'redux';
 
-export const PATH_BASE = 'https://hn.algolia.com/api/v1';
+export const PATH_BASE = 'https://images-api.nasa.gov/search';
 export const PATH_SEARCH = '/search';
-export const PARAM_SEARCH = 'query=';
+export const PARAM_SEARCH = 'q=';
 export const PARAM_PAGE = 'page=';
